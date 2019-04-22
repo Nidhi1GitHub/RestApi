e1089a2916c04729262f23f4840f52bfde0e42ad
Nidhi <developer.dhavalp@gmail.com>
2019-04-22 11:44:03 +0530

    .

---
+++ RestApi/Controllers/ContactController.cs
@@ -0,0 +1,59 @@
+﻿using RestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RestApi.Services;
+
+namespace RestApi.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        //http://localhost:54970/api/Get
+        //public string[] Get()
+        //{
+        //    return new string[]
+        //    {
+        //        "Hello",
+        //        "World"
+        //    };
+        //}
+
+        //http://localhost:54970/api/Get
+        //[Route("api/Get")]
+        //public Contact[] Get()
+        //{
+        //    return new Contact[]
+        //    {
+        //        new Contact
+        //        {
+        //            Id = 1,
+        //            Name = "Nidhi"
+        //        },
+        //        new Contact
+        //        {
+        //            Id = 2,
+        //            Name = "Krish"
+        //        }
+        //    };
+        //}
+
+        //http://localhost:54970/api/Get
+
+        [Route("api/Get")]
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+
+    }
+}
---
+++ RestApi/Services/ContactRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestApi.Models;
+
+namespace RestApi.Services
+{
+    public class ContactRepository
+    {
+        public Contact[] GetAllContacts()
+        {
+            return new Contact[]
+            {
+                new Contact
+                {
+                    Id = 1,
+                    Name = "Naineta"
+                },
+                new Contact
+                {
+                    Id = 2,
+                    Name = "Krish"
+                }
+            };
+        }
+    }
+}
