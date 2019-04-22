f3e145cbf0e4b7a1b3aa41b6e19034f02b9d5257
Nidhi <developer.dhavalp@gmail.com>
2019-04-22 14:11:08 +0530

    .

---
+++ RestApi/Controllers/StudentRetriveController.cs
@@ -0,0 +1,30 @@
+﻿using RestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestApi.Controllers
+{
+    public class StudentRetriveController : ApiController
+    {
+        public List<Student> GetAllStudents()
+        {
+            return StudentRegistration.getInstance().getAllStudent();
+        }
+        [Route("api/studentregistration")]
+        public StudentRegistrationReply registerStudent(Student studentregd)
+        {
+            Console.WriteLine("In registerStudent");
+            StudentRegistrationReply stdregreply = new StudentRegistrationReply();
+            StudentRegistration.getInstance().Add(studentregd);
+            stdregreply.Name = studentregd.Name;
+            stdregreply.Age = studentregd.Age;
+            stdregreply.RegistrationNumber = studentregd.RegistrationNumber;
+            stdregreply.RegistrationStatus = "Successful";
+            return stdregreply;
+        }
+    }
+}
